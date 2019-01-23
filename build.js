6ee5e98b82bb01568f8eb7d4e812f0844238d1c0
citycide <lingenbw@gmail.com>
2019-01-23 09:38:38 -0600

    chore: use built-in terser plugin, improve build script output

--- build.js
+++ build.js
@@ -1,7 +1,9 @@
 'use strict'
 
+const { stat } = require('fs').promises
+const { join } = require('path')
+
 const webpack = require('webpack')
-const Terser = require('terser-webpack-plugin')
 
 const webpackConfig = {
   mode: 'production',
@@ -22,12 +24,6 @@ const webpackConfig = {
   node: {
     fs: 'empty'
   },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new Terser()
-    ]
-  },
   output: {
     library: 'Babel',
     libraryTarget: 'umd'
@@ -39,7 +35,7 @@ const main = config => new Promise((resolve, reject) => {
 })
 
 const build = async (src, dest) => {
-  console.log(`building from ${src}...`)
+  console.log(`${src} -> ${dest}`)
   const config = { ...webpackConfig, entry: `./src/${src}` }
   config.output.filename = dest
 
@@ -49,11 +45,27 @@ const build = async (src, dest) => {
   }
 
   try {
-    console.log(`outputting ${dest}...`)
     const stats = await main(config)
-    console.log(stats.toJson('minimal'))
+    const json = stats.toJson('minimal')
+
+    if (stats.hasErrors()) {
+      console.error(json.errors)
+    } else {
+      const { size } = await stat(join('dist', dest))
+      console.log(`${dest} created @ ${size / 1000000.0} MB`)
+    }
+
+    if (stats.hasWarnings()) {
+      json.warnings
+        .filter(warning =>
+          !warning.includes('size limit') &&
+          !warning.startsWith('webpack performance recommendations')
+        )
+        .forEach(warning => console.warn('\n', warning, '\n'))
+    }
   } catch (e) {
-    console.error(e)
+    console.error(e.stack || e)
+    e.details && console.error(e.details)
   }
 }
 
